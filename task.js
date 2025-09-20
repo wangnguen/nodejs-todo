7f8bd2bf264d39a24b157036c7302e16bc9eb46f
WangNguen <wangnguenlc79@gmail.com>
2025-09-20 14:37:11 +0700

    feat: add rest api crud task

--- task.js
+++ task.js
@@ -4,67 +4,55 @@ const path = require("path");
 const filePath = path.join(__dirname, "db", "tasks.json");
 
 const saveTasksToFile = async (tasks) => {
-	try {
-		const dataJson = JSON.stringify(tasks, null, 2);
-		await fs.writeFile(filePath, dataJson, "utf8");
-		console.log("Ghi file thành công !");
-	} catch (err) {
-		console.log("Lỗi không ghi được: " + err);
-	}
+	const dataJson = JSON.stringify(tasks, null, 2);
+	await fs.writeFile(filePath, dataJson, "utf8");
+	// Không swallow lỗi, để lỗi propagate lên caller
 };
 
 const loadTasksFromFile = async () => {
-	try {
-		const data = await fs.readFile(filePath, "utf8");
-		return JSON.parse(data);
-	} catch (err) {
-		console.log("Lỗi không đọc được: " + err);
-		return [];
-	}
+	const data = await fs.readFile(filePath, "utf8");
+	return JSON.parse(data);
+	// Nếu lỗi sẽ throw lên caller
 };
 
 const createTask = async (newTask) => {
 	const tasks = await loadTasksFromFile();
-	const existTask = tasks.find((task) => task.id === newTask.id);
-	if (!existTask) {
-		tasks.push(newTask);
-		await saveTasksToFile(tasks);
-		console.log("Tạo mới task thành công !");
+	const newId = +newTask.id;
+	const existTask = tasks.find((task) => +task.id === newId);
+	if (existTask) {
+		throw new Error("Id đã tồn tại !");
 	}
+	const taskToStore = Object.assign({}, newTask, { id: newId });
+	tasks.push(taskToStore);
+	await saveTasksToFile(tasks);
+	return taskToStore;
 };
 
 const getTaskById = async (id) => {
 	const tasks = await loadTasksFromFile();
-	const existTask = tasks.find((task) => task.id === id);
-
-	if (existTask) {
-		console.log(existTask);
-		return existTask;
-	} else {
-		console.log("Không tìm thấy task !");
-		return null;
-	}
+	const existTask = tasks.find((task) => +task.id === +id);
+	return existTask || null;
 };
 
 const updateTask = async (updateTask) => {
 	const tasks = await loadTasksFromFile();
-	const existTask = tasks.find((task) => task.id === updateTask.id);
-
+	const id = +updateTask.id;
+	const existTask = tasks.find((task) => +task.id === id);
 	if (existTask) {
-		Object.assign(existTask, updateTask);
-		saveTasksToFile(tasks);
-		console.log("Update task thành công !");
+		Object.assign(existTask, Object.assign({}, updateTask, { id }));
+		await saveTasksToFile(tasks);
+		return existTask;
 	}
+	return null;
 };
 
-const deleteTask = async (deleteTask) => {
+const deleteTask = async (id) => {
 	const tasks = await loadTasksFromFile();
-	const newTasks = tasks.filter((task) => task.id != deleteTask.id);
-
-	if (newTasks) {
-		saveTasksToFile(newTasks);
-		console.log("Xóa task thành công !");
-	}
+	const existTask = tasks.filePath((t) => t.id === +id);
+	if (!existTask) return false;
+	const newTasks = tasks.filter((t) => t.id !== +id);
+	await saveTasksToFile(newTasks);
+	return true;
 };
 
 module.exports = {
