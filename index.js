e433580960ea8c3acac4bd9dfb179858b3bc57ef
WangNguen <wangnguenlc79@gmail.com>
2025-09-26 11:32:00 +0700

    feat: add connect to db

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const app = require("./src/app");
+const db = require("./src/database/models/index");
 
 const PORT = 3000;
+
+(async () => {
+	try {
+		await db.sequelize.authenticate();
+		console.log("Kết nối DB thành công !");
+	} catch (error) {
+		console.log("Kết nối thất bại: " + error.message);
+	}
+})();
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
